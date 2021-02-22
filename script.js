695cabd1d4f3c7a79a3bce2f8bb6af6aa25ff95b
envy124 <pkgsail.dev@gmail.com>
2021-02-22 12:02:46 +0300

    add toto checkbox click handler

--- script.js
+++ script.js
@@ -2,7 +2,12 @@ const todos = [];
 const input = $('#task_value');
 let idGen = 0;
 
-
+function todoCheckboxClicked(event) {
+    const todoId = event.data.todoId;
+    const checked = event.target.checked;
+    const todo = todos.find(todo => todo.id === todoId);
+    todo.status = checked;
+}
 
 function addTodo() {
     let task = input.val();
@@ -23,8 +28,12 @@ function addTodo() {
 
     const ul = $('#listToDo');
     ul.html('');
-    todos.forEach(todo => {
-        ul.append(`<li class = "task-li" id=${todo.id}><div><input type="checkbox" ${todo.status ? 'checked' : ''}>${todo.text}<button class = "del-btn">delete</button></div></li>`);
+    todos.forEach((todo, index) => {
+        const selector = ul.append(`<li class = "task-li" id=${todo.id}><div><input type="checkbox" ${todo.status ? 'checked' : ''}>${todo.text}<button class = "del-btn">delete</button></div></li>`);
+        // const todoHtmlElem = selector[0].children[index];
+        // const checkBox = todoHtmlElem.children[0].children[0];
+        $(`#${todo.id} div input`).on('click', { todoId: todo.id }, todoCheckboxClicked);
+        // debugger;
     });
 }
 
